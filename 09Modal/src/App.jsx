71c96403cef324c7bb000cf3a4c61ea45260ee8c
SalmanHossen <hossainsalman873@gmail.com>
2024-07-12 11:31:36 +0600

    modal folder structure done

---
+++ 09Modal/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center bg-gray-700 text-white text-3xl p-4 mt-8'>Modal Practice using React and Tailwind css</h1>
+      
+      <div>
+
+      </div>
+      
+      <div>
+
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default App;
